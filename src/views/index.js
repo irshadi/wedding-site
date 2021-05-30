dbe0b584235465a8ff902bc03f9e824efe25e636
irshadi <bagasputro.irshadi@gmail.com>
2021-05-30 20:40:00 +0700

    feat: initialize main page and add chakraui

---
+++ src/views/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Header } from "./Header";
+
+export const MainPage = () => {
+  return (
+    <React.Fragment>
+      <Header />
+      HEHEHE
+    </React.Fragment>
+  );
+};
