8f3b300d215b33e208cb677d766b9d424dc7240b
irshadi <bagasputro.irshadi@gmail.com>
2021-06-13 14:38:44 +0700

    feat: add 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,54 +1,24 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import { Flex } from "@chakra-ui/layout";
+import { Heading } from "@chakra-ui/layout";
+import { Text } from "@chakra-ui/layout";
+import { Button } from "@chakra-ui/button";
+import { Link } from "gatsby";
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-
-// markup
 const NotFoundPage = () => {
   return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
-  )
-}
+    <Flex w="100%" h="40em" justify="center" flexDir="column">
+      <Heading textAlign="center" fontSize="15em">
+        404
+      </Heading>
+      <Text textAlign="center" fontFamily="georgia" fontSize="2em">
+        {"¯_(ツ)_/¯"} Oops....
+      </Text>
+      <Text textAlign="center" fontFamily="georgia" mt="3em">
+        Page not Found
+      </Text>
+    </Flex>
+  );
+};
 
-export default NotFoundPage
+export default NotFoundPage;
